a9cae67ff2e8b6aca7adb93dae427f8ccaba0af9
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2025-03-11 22:20:01 +0000

    postgres support

--- src/charm.py
+++ src/charm.py
@@ -19,9 +19,21 @@ import logging
 import socket
 
 import ops
+from charms.data_platform_libs.v0.data_interfaces import DatabaseRequires
 from charms.traefik_k8s.v2.ingress import IngressPerAppRequirer
 
 logger = logging.getLogger(__name__)
+
+
+DB_NAME = "joplin_db"
+
+
+class BlockedError(Exception):
+    pass
+
+
+class WaitingError(Exception):
+    pass
 
 
 class JoplinServerCharm(ops.CharmBase):
@@ -31,22 +43,36 @@ class JoplinServerCharm(ops.CharmBase):
         self.ingress = IngressPerAppRequirer(self, port=22300, strip_prefix=True)
         framework.observe(self.on['joplin-server'].pebble_ready, self.configure)
         framework.observe(self.on['ingress'].relation_changed, self.configure)
+        self.database = DatabaseRequires(self, relation_name='database', database_name=DB_NAME)
+        framework.observe(self.database.on.database_created, self.configure)
+        framework.observe(self.database.on.endpoints_changed, self.configure)
 
     def configure(self, event: ops.PebbleReadyEvent):
-        self.unit.open_port(protocol='tcp', port=22300)
+        try:
+            self.unit.open_port(protocol='tcp', port=22300)
 
-        container = self.unit.get_container('joplin-server')
-        if not container.can_connect():
-            return
+            container = self.unit.get_container('joplin-server')
+            if not container.can_connect():
+                raise WaitingError('Waiting for container to start')
 
-        container.add_layer("joplin-server", self.joplin_server_layer(), combine=True)
-        container.autostart()
-        container.replan()
-        self.unit.set_ports(22300)
+            container.add_layer('joplin-server', self.joplin_server_layer(), combine=True)
+            container.autostart()
+            container.replan()
+            self.unit.set_ports(22300)
 
-        self.unit.status = ops.ActiveStatus()
+            self.unit.status = ops.ActiveStatus()
+        except BlockedError as e:
+            self.unit.status = ops.BlockedStatus(str(e))
+        except WaitingError as e:
+            self.unit.status = ops.WaitingStatus(str(e))
 
     def joplin_server_layer(self):
+        env = {
+            'APP_BASE_URL': self.config.get("external-url") or self.ingress.url or socket.getfqdn(),
+            'APP_PORT': '22300',
+            'DB_CLIENT': 'pg',
+        }
+        env.update(self.postgres_data())
         return {
             'summary': 'Joplin Server Layer',
             'description': 'Pebble layer for Joplin Server',
@@ -55,16 +81,33 @@ class JoplinServerCharm(ops.CharmBase):
                     'override': 'replace',
                     'command': self.command(),
                     'startup': 'enabled',
-                    'environment': {
-                        'APP_BASE_URL': self.ingress.url or socket.getfqdn(),
-                        'APP_PORT': '22300',
-                    },
+                    'environment': env,
                 }
             }
         }
 
     def command(self):
         return 'tini -- yarn start-prod'
+
+    def postgres_data(self):
+        '''Fetch postgres relation data.'''
+
+        relations = self.database.fetch_relation_data().values()
+        if len(relations) < 1:
+            raise BlockedError('Postgres relation required')
+        if len(relations) > 1:
+            raise Exception('Multiple charms related over the database relation but limit is one')
+        data = list(relations)[0]
+        if not data:
+            raise WaitingError('Waiting for DB info.')
+        host, port = data['endpoints'].split(':')
+        return {
+            'POSTGRES_DATABASE': DB_NAME,
+            'POSTGRES_HOST': host,
+            'POSTGRES_PORT': port,
+            'POSTGRES_USER': data['username'],
+            'POSTGRES_PASSWORD': data['password'],
+        }
 
 
 if __name__ == '__main__':
